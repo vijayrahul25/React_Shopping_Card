5cb5c239f52a9a1ed1e143c909161462a29213d5
Rahul vijayvergiya <vijayrahul25@gmail.com>
2018-02-21 13:56:01 +0530

    jquery popup cart

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,27 +1,26 @@
+import 'react-polyfill';
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class Cart extends Component {
+  
   render() {
     let cartItems;
     if (this.props.totalItems > 0 ) {
       cartItems = this.props.cartItems.map(product => {
         return (
-          <ul className=" list-unstyled unstyled">
-          <li className="cart-item" key={product.name}>
-            <div class="row">
-              <div class="col-sm-4"><img width="25px" className="product-image" src={product.image} />{product.name}</div>
+          <div class="row" key={product.id}>       
+              <div class="col-sm-4"><img width="25px" className="product-image" src={product.image} /> {product.name}</div>
               <div class="col-sm-2">{product.price}</div>
-              <div class="col-sm-2"><p className="quantity">{product.quantity} {product.quantity > 1 ? "Nos." : "No."} </p></div>
+              <div class="col-sm-3"><p className="quantity">{product.quantity} {product.quantity > 1 ? "Nos." : "No."} </p></div>
               <div class="col-sm-2"><p className="amount">{product.quantity * product.price}</p></div>
-            </div>           
-          </li>
-          </ul>
+              </div>
+             
         )
       });
     } 
-    return (<div>
-      { cartItems}
+    return (<div class="cart-container"> 
+      {cartItems}
       </div>
     )
   }
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,22 +2,43 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Cart from "./Cart.js";
+import { findDOMNode } from "react-dom";
+import $ from 'jquery';
 
 class Header extends Component {
+  handleToggle = () => {
+    const el = findDOMNode(this.refs.toggle);
+    $(el).slideToggle();
+  };
   render() {
     return (
       <nav>
         <div class="container">
-          <ul class="navbar-left unstyled">
-            <li>
-              <a href="#" id="cart">
+         
+          <a href="#" id="cart" onClick={this.handleToggle}>
                 <i class="glyphicon glyphicon-shopping-cart" /> Cart{" "}
                 <span class="badge">{this.props.totalItems}</span>
               </a>
+              <div 
+                style={{
+                  position: 'absolute',
+                  backgroundColor: '#EEE',
+                  boxShadow: '0 5px 10px rgba(0, 0, 0, 0.2)',
+                  border: '1px solid #CCC',
+                  borderRadius: 3,
+                  marginLeft: -5,
+                  marginTop: 5,
+                  padding: 10,
+                  display:'none'                  ,
+                  zIndex: 10,
+                  
+                }}
+                ref='toggle'
+              >
               <Cart totalItems={this.props.totalItems} cartItems={this.props.cartItems} />
-            </li>
-          </ul>
-          
+              
+          </div>
+             
           <hr />
         </div>
       </nav>
